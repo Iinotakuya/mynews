a920ae5987e353d871a60948ac16180dd183df3f
iino <tky0134@gmail.com>
2020-07-11 10:24:04 +0000

    09課題

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,23 @@
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('news/create','Admin\NewsController@add');
+});*/
+
+/*3.「http://XXXXXX.jp/XXX というアクセスが来たときに、 AAAControllerのbbbというAction に渡すRoutingの設定」を書いてみてください。*/
+Route::get('xxx','AAAController\bbb@');
+
+
+/*4.【応用】 前章でAdmin/ProfileControllerを作成し、add Action, edit Actionを追加しました。
+web.phpを編集して、admin/profile/create にアクセスしたら ProfileController の add Action に、
+admin/profile/edit にアクセスしたら ProfileController の edit Action に割り当てるように設定してください。*/
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('profile/create','Admin\ProfileController@add');
+    Route::get('profile/edit','Admin\ProfileController@edit');
+});
+
